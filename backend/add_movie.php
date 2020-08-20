9a2e2b8860d8fd602f09ddf7554b7a5015717e36
張 建文 <chang180@gmail.com>
2020-08-20 10:27:02 +0800

    後台功能全部完成

---
+++ backend/add_movie.php
@@ -0,0 +1,36 @@
+<form action="api/save_movie.php" method="post" enctype="multipart/form-data">
+片名：<input type="text" name="name"><br>
+分級：<select name="level">
+    <option value="1">普遍級</option>
+    <option value="2">保護級</option>
+    <option value="3">輔導級</option>
+    <option value="4">限制級</option>
+</select>
+片長：<input type="text" name="length"><br>
+上映日期：
+<select name="y"><option value="2020">2020</option>
+</select>年
+<select name="m">
+    <?php
+for($i=1;$i<=12;$i++){
+    echo "<option>$i</option>";
+}
+?>
+</select>月
+<select name="d">
+    <?php
+for($i=1;$i<=31;$i++){
+    echo "<option>$i</option>";
+}
+?>
+</select>日
+<br>
+發行商：<input type="text" name="publish"><br>
+導演：<input type="text" name="director"><br>
+預告影片：<input type="file" name="trailer"><br>
+電影海報：<input type="file" name="poster"><br>
+劇情簡介：<textarea name="intro" style="width:500px;height:200px;"></textarea><br>
+排序：<input type="number" name="rank"><br>
+是否顯示(1:顯示，0:不顯示) <input type="number" name="sh"><br>
+<div class="ct"><button>新增</button><button type="reset">重置</button></div>
+</form>
